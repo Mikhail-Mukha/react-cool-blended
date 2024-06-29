c052323a656b1f032294aedf95c6f2c30a420944
Halyna-Mukha <msaarf555@gmail.com>
2024-06-29 12:04:09 +0200

    task-4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,27 @@
-// const tableHeaders = ['№', 'price', 'amount', 'date'];
+const tableHeaders = ['№', 'price', 'amount', 'date'];
+import formatDate from 'helpers/formatDate';
+import style from './CryptoHistory.module.css'
+export const CryptoHistory = ({ items }) => {
+  return (
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          {tableHeaders.map((element, index) => (<th key={index} className={style.th}>{element}</th>))}
+          
+        </tr>
+      </thead>
 
-export const CryptoHistory = () => {
-  return <h2>CryptoHistory</h2>;
+      <tbody>
+        {items.map(({amount, date, id, price}, index) => 
+      <tr key={id} className={style.tr}>
+        <td className={style.td}>{index+1}</td>
+        <td className={style.td}>{price}</td>
+        <td className={style.td}>{amount}</td>
+        <td className={style.td}>{formatDate(date)}</td>
+      </tr>
+        )}
+        
+      </tbody>
+    </table>
+  );
 };
---
+++ src/helpers/formatDate.js
@@ -0,0 +1,11 @@
+import { format } from "date-fns"
+
+
+
+const formatDate = (date) => {
+    return (
+        format(new Date(date), "Pp")
+    )
+}
+
+export default formatDate
